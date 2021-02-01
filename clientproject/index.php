824e0a61fae6d4b380f6b5ce119d869ee3bca494
cameronzipp <78177750+cameronzipp@users.noreply.github.com>
2021-02-01 14:49:33 -0800

    Update and rename index.html to index.php

---
+++ clientproject/index.php
@@ -0,0 +1,140 @@
+<?php
+    include ('includes/head.html');
+?>
+<body>
+<?php
+include ('includes/jumbohead.html');
+include ('includes/nav.html');
+?>
+
+
+<!--bootstrap card with logos-->
+<div class="album py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 d-flex align-items-stretch">
+                <div class="card">
+                    <img class="card-img-top" src="images/auroratech.png" height="250"  alt="Card image cap"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Aurora Tech</h5>
+                        <p class="card-text">Technology company that is building the world’s driver and its ecosystem
+                            to save lives, get crucial goods where they need to go, and make mobility more efficient
+                            and accessible for all.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="https://aurora.tech/"><button type="button" class="btn btn-sm btn-outline-secondary">Visit</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-items-stretch">
+                <div class="card">
+                    <img class="card-img-top" src="images/allpowerlabs.png" height="250"  alt="Card image cap"/>
+                    <div class="card-body">
+                        <h5 class="card-title">All Power Labs</h5>
+                        <p class="card-text">Astrolabe Analytics’ software makes batteries work better, longer and more
+                            reliably, using a suite of cloud computing, data management and machine learning techniques.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="http://www.allpowerlabs.com/"><button type="button" class="btn btn-sm btn-outline-secondary">Visit</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-items-stretch">
+                <div class="card">
+                    <img class="card-img-top" src="images/oValley1.png" height="250"  alt="Card image cap"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Organic Valley</h5>
+                        <p class="card-text">Conglomerate of local agriculture co-ops in the Midwest.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="https://www.organicvalley.coop/"><button type="button" class="btn btn-sm btn-outline-secondary">Visit</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 d-flex align-items-stretch">
+                <div class="card">
+                    <img class="card-img-top" src="images/Nuleaf1.png" height="250"  alt="Card image cap"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Nuleaf</h5>
+                        <p class="card-text">NuLeaf’s NuTree makes treating and recycling water at any scale easy by
+                            mimicking wetland ecosystems.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="https://www.nuleaftech.com/"><button type="button" class="btn btn-sm btn-outline-secondary">Visit</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-items-stretch">
+                <div class="card">
+                    <img class="card-img-top" src="images/Evmatch.jpg" height="250"  alt="Card image cap"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Evmatch</h5>
+                        <p class="card-text">EVmatch’s peer-to-peer network for EV charging harnesses the power of
+                            sharing to address the need for more reliable and convenient charging.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="https://www.evmatch.com/"><button type="button" class="btn btn-sm btn-outline-secondary">Visit</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-items-stretch">
+                <div class="card">
+                    <img class="card-img-top" src="images/aquacycl.png" height="250"  alt="Card image cap"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Aquacycl</h5>
+                        <p class="card-text">Aquacycl uses bacteria to convert organic waste into
+                            electricity, while decentralizing wastewater treatment for industries and applications.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="https://www.aquacycl.com/"><button type="button" class="btn btn-sm btn-outline-secondary">Visit</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!--End Nav-->
+
+<br><br><br><br><br>
+
+<!-- Start:Footer bottom -->
+
+<div class="footerBottom">
+    <div class="mainContainer">
+        <p>Copyright © 2021 CONEYBEARE, LLC and LEAN IS GREEN. All rights reserved</p>
+    </div>
+</div>
+
+<!-- End:Footer bottom -->
+
+
+
+
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+<!--<script src="scripts/scripts.js"></script>-->
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+-->
+
+</body></html>
